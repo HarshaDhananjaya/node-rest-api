2f4255759f20d8a27096a88058fda0579c8f382c
Harsha Liyanage <harsha@mediwave.io>
2025-02-22 21:29:49 +0530

    Add validation middleware for user creation and update routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const validateUser = require("../middlewares/validateUser"); // Import validation middleware
 
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUser);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
+
+// Apply validation middleware for POST and PUT requests
+router.post("/", validateUser, userController.createUser);
+router.put("/:id", validateUser, userController.updateUser);
+
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
